24f27da7666a46e544b44b5b3521ecda5bce0a12
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-30 10:27:34 -0500

    fixes book_by_id route

--- server/routes/book_by_id.py
+++ server/routes/book_by_id.py
@@ -2,17 +2,19 @@ from . import request, Resource
 from models.book import Book
 from schemas.book_schema import BookSchema
 from app_setup import db
+from decorators import require_librarian
 
 book_schema = BookSchema(session=db.session)
 
 
 class BookById(Resource):
-    def get(self, ISBN):
-        if book := db.session.query(Book).filter_by(ISBN == ISBN).first():
+    def get(self, id):
+        if book := db.session.get(Book, id):
             book_schema = BookSchema()
             return book_schema.dump(book), 200
         return {"error": "Could not find book"}, 404
 
+    @require_librarian
     def delete(self, id):
         if book := db.session.get(Book, id):
             try:
@@ -23,15 +25,3 @@ class BookById(Resource):
                 db.session.rollback()
                 return {"error": str(e)}, 400
         return {"error": "Could not find Book"}, 404
-
-    #!Optional: Allows librarians to remove book by ISBN
-    # def delete(self, ISBN):
-    #     if book := db.session.query(Book).filter_by(ISBN == ISBN).first():
-    #         try:
-    #             db.session.delete(book)
-    #             db.session.commit()
-    #             return {"message": f"Book {id} has been deleted"}, 200
-    #         except Exception as e:
-    #             db.session.rollback()
-    #             return {"error": str(e)}, 400
-    #     return {"error": "Could not find book"}, 404
--- server/routes/overdue_books.py
+++ server/routes/overdue_books.py
@@ -3,10 +3,12 @@ from models.book import Book
 from schemas.book_schema import BookSchema
 from app_setup import db
 from datetime import datetime
+from decorators import require_librarian
 
 books_schema = BookSchema(many=True, session=db.session)
 
 class OverdueBooks(Resource):
+    @require_librarian
     def get(self):
         overdue_books = db.session.query(Book).filter(Book.due_date < datetime.utcnow(), Book.checked_out == True).all()
         overdue_books_data = books_schema.dump(overdue_books)
