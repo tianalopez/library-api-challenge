98df839e5db4a2e08d27c6cc7ac69278d8211ce8
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-30 10:52:49 -0500

    fixes checked out route

--- server/routes/checked_out_books.py
+++ server/routes/checked_out_books.py
@@ -7,6 +7,6 @@ from app_setup import db
 books_schema = BookSchema(many=True, session=db.session)
 
 class CheckedOutBooks(Resource):
-    def get(self, user_id):
-        checked_out_books = books_schema.dump(Book.query.filter_by(user_id=user_id))
+    def get(self, id):
+        checked_out_books = books_schema.dump(Book.query.filter_by(user_id=id, checked_out=True))
         return checked_out_books, 200
